e0a31589faf35b7384bbd7a5d6d1054f462db4da
Brandt Lareau <brandt@lareaus.com>
2024-09-13 13:23:14 -0700

    Guard against nil or blank values

--- lib/openkick/model/class_methods.rb
+++ lib/openkick/model/class_methods.rb
@@ -96,7 +96,7 @@ module Openkick
           define_method(:"reindex_#{association}") do
             return unless Openkick.callbacks?
 
-            send(association).reindex(partial)
+            send(association)&.reindex(partial)
           end
         end
         if respond_to?(:after_commit)
